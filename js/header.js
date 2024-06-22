1a08059ad638467db2cdd9c918bb5e9053f3610f
Axel Higa <axeltomashiga@gmail.com>
2024-06-22 14:08:37 -0300

    ordenamiento
    modifico el menu hamburguesa y algunas cosas de plantila juego

--- js/header.js
+++ js/header.js
@@ -26,30 +26,30 @@ header.append(`
             </div>
             <div class="offcanvas-body">
                 <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="productos.html" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-dark">
-                    <li><a class="dropdown-item" href="">Action</a></li>
-                    <li><a class="dropdown-item" href="productos.html">Another action</a></li>
-                    <li>
-                        <hr class="dropdown-divider">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.html">Inicio</a>
                     </li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.html#banner">Subscripciones</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="productos.html" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Categorias
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-dark">
+                            <li><a class="dropdown-item" href="productos.html#accion">Action</a></li>
+                            <li><a class="dropdown-item" href="productos.html#deportes">Sports</a></li>
+                            <li><a class="dropdown-item" href="productos.html#disparos">Shooter</a></li>
+                            <li><a class="dropdown-item" href="productos.html#horror">Horror</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="faq.html">Soporte</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contacto.html">Contacto</a>
+                    </li>
                 </ul>
-                <form class="d-flex mt-3" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-success" type="submit">Search</button>
-                </form>
             </div>
             </div>
         </div>
