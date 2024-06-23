42930a08ccea8fd5e29d0e74361deeb7a0d9cf87
Axel Higa <axeltomashiga@gmail.com>
2024-06-23 17:32:56 -0300

    arreglo de las categorias

--- js/header.js
+++ js/header.js
@@ -9,10 +9,10 @@ header.append(`
     <a id="boton_header_subscripciones" class="col p-md-1 me-xl-4 boton_navegacion d-none d-xl-block" href="index.html#banner">Subscripciones</a>
     <a class="col p-md-1 me-xl-4 boton_navegacion d-none d-xl-block   dropdown-toggle"  role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">Categorias</a>
     <ul class="dropdown-menu w-25" aria-labelledby="dropdownMenuLink">
-        <li><a class="dropdown-item" href="productos.html">Action</a></li>
-        <li><a class="dropdown-item" href="productos.html#deportes">Sports</a></li>
-        <li><a class="dropdown-item" href="productos.html#disparos">Shooter</a></li>
-        <li><a class="dropdown-item" href="productos.html#horror">Horror</a></li>
+        <li><a class="dropdown-item" href="productos.html#action-section">Action</a></li>
+        <li><a class="dropdown-item" href="productos.html#sport-section">Sports</a></li>
+        <li><a class="dropdown-item" href="productos.html#shooter-section">Shooter</a></li>
+        <li><a class="dropdown-item" href="productos.html#horror-section">Horror</a></li>
     </ul>
     <a class="col p-md-1 me-xl-4 boton_navegacion d-none d-xl-block" href="faq.html">Soporte</a>
     <a class="col p-md-1 me-xl-4 boton_navegacion d-none d-xl-block" href="contacto.html">Contacto</a> 
@@ -39,10 +39,10 @@ header.append(`
                         Categorias
                         </a>
                         <ul class="dropdown-menu dropdown-menu-dark">
-                            <li><a class="dropdown-item" href="productos.html#accion">Action</a></li>
-                            <li><a class="dropdown-item" href="productos.html#deportes">Sports</a></li>
-                            <li><a class="dropdown-item" href="productos.html#disparos">Shooter</a></li>
-                            <li><a class="dropdown-item" href="productos.html#horror">Horror</a></li>
+                            <li><a class="dropdown-item" href="productos.html#action-section">Action</a></li>
+                            <li><a class="dropdown-item" href="productos.html#sport-section">Sports</a></li>
+                            <li><a class="dropdown-item" href="productos.html#shooter-section">Shooter</a></li>
+                            <li><a class="dropdown-item" href="productos.html#horror-section">Horror</a></li>
                         </ul>
                     </li>
                     <li class="nav-item">
